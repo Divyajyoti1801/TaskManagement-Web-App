9f75d588f0feeb1ee6c4efcc0688d5159a879270
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-04-24 23:44:38 +0530

    PROJECT RESET

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_DB)
-    .then((DB) => console.log(`Database Connected: ${DB.connection.host}`));
-};
-
-export default connectDB;
--- backend/middleware/asyncErrorHandler.js
+++
@@ -1,5 +0,0 @@
-const asyncErrorHandler = (func) => (req, res, next) => {
-  Promise.resolve(func(req, res, next)).catch(next);
-};
-
-export default asyncErrorHandler;
