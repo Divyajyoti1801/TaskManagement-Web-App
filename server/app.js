66621b286e1092ab6b8b31a9b865be35c8fa9ab0
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-05-08 14:42:48 +0530

    Bug Solving Test-02

--- server/app.js
+++ server/app.js
@@ -10,10 +10,10 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-app.use(express.urlencoded({ extended: false }));
 
 //Major Routes Imports
 import boardRouter from "./routes/BoardRoutes.js";
